4f5669f455bfaea3f0a9c2e201529d446e9ae154
Ben Van <send2shawn@yahoo.com>
2020-12-08 15:29:12 -0500

    Update rokt.sh
    V1.0  The last update to the ROKT.  See the release notes.

--- rokt.sh
+++ rokt.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/bash
-# rokt9cx 
+# rokt.v1.0
 # ========================================
 function loadExternalValidators {
+
 
 # = = = = manually edit / insert external nodes into eVALS list: max = 15
 # format = 
 #             <doublequote>
 # Validator address <comma>
-#   Refrence number <comma>    { any two digit number EXCEPT 00 or 99 }
+#   Refrence number <comma>    { any two digit number between 01 and 30 }
 #         Nick name <comma>    { any 5 chars }
-#     Validator URL <comma>    { just the URL no "https://" prefix no ":port" sufix }
+#	  User name <coma>     { used to ssh into remote machine }
+#     Validator URL <comma>    { IP or the URL no "https://" prefix no ":port" sufix }
 #               unk <comma>    { this makes space for the block height to be tracked }
 #         unk <doublequote>    { this makes space for the node status to be tracked }
 #
 # Sample:
-#         "e09ce22e0abfd8129776128c0c9b3836024d8c6e,01,POKT1,node1.mainnet.pocket.network,unk,unk"
-# some preloaded nodes below... replace or remove them... 
-# = = = = 
+#         "e09ce22e0abfd8129776128c0c9b3836024d8c6e,01,POKT1,username,node1.mainnet.pocket.network,unk,unk"
+
         eVALS=(
-        "e09ce22e0abfd8129776128c0c9b3836024d8c6e,01,POKT1,node1.mainnet.pocket.network,unk,unk"
-        "8dd722c42425783b50db707995f841b3c7ccc827,02,POKT2,node2.mainnet.pocket.network,unk,unk"
-        "8afc6b4195e3fd59fa3aa8bab65b2b7c497cedf9,03,POKT3,node3.mainnet.pocket.network,unk,unk"
+"e09ce22e0abfd8129776128c0c9b3836024d8c6e,01,POKT1,username,node1.mainnet.pocket.network,unk,unk"
+
+
 )
 }
 # =========================================
@@ -57,14 +58,13 @@ function setCursorConstants {
 # ========================================
 function error_exit()
 {
-  echo "$1" 1>&2
-  if $breakOnError ; then  echo "Aborting";exit 1; fi
+#  echo "$1" 1>&2
+  if $breakOnError ; then echo "$1" 1>&2; echo "Aborting";exit 1; fi
 }
 # =========================================
 function initWindow {
 	$(resize -s 40 100 >/dev/null )  
        echo -e ${CGH}${CCD}      
-#      echo -e "\033[?3l" #set 80 cols?  no apparent effect.
 }
 # ===========================================
 function setGlobalVars {
@@ -92,7 +92,7 @@ lscreen=(
 	"STK,C,8,65,21,1,CurSTK,Staked: "
 	"AUT,N,9,65,21,1,CurAUT,Restarts: "
 	"24B,NC,8,38,24,1,Cur24B,24h Chg: "
-	"VVV,AS,12,2,26,16,eVALS,"
+	"VVV,AS,12,2,26,31,eVALS,"
 	"NUM,N,5,43,20,1,CurNUM,Staked Vals: "
 	"NUM,N,5,66,20,1,CurAPP,Staked Apps: "
 	"TIM,MS,5,3,30,1,CurTIM,Next Block in: "
@@ -100,7 +100,7 @@ lscreen=(
 	"CHU,AD,12,36,27,16,CurCHU,= "
         )
 lscreentext=(
-	"0,1,ROKT v0.9.c.x - Ben@BenVan.com 2020/10/12 - Free and Open Source... enjoy!"
+	"0,1,ROKT v1.0 - Ben@BenVan.com 2020/Dec/08 - Free and Open Source... enjoy!"
 	"10,4,External Validators "
 	"10,38,local chains.json "
 	"11,30,Chain =  URL"
@@ -110,7 +110,7 @@ lscreentext=(
         )
 
 qscreentext=(
-        "0,1,ROKT v0.9.c.x - Ben@BenVan.com 2020/10/12 - Free and Open Source... enjoy!"
+        "0,1,ROKT v1.0 - Ben@BenVan.com 2020/Dec/08 - Free and Open Source... enjoy!"
 	"3,5,Queries (see stuff)"
 	"3,33,Actions (do stuff)"
 	"3,68,Changes (break stuff)"
@@ -121,7 +121,7 @@ qscreentext=(
 	"6,30,STA - ${CYA}sta${NC}rt Pocket- in Background"
 	"5,66,CHA - edit ${CYA}cha${NC}ins.json"
 	"7,30,SIM - start Pocket- ${CYA}sim${NC}ulateRelay"
-	"8,30,RES - Pocket- ${CYA}Res${NC}et"
+	"8,30,RES - Pocket- ${CYA}Res${NC}et[ Danger! ]"
 	"6,66,CON - edit ${CYA}con${NC}fig.json"
 	"7,66,GIN - edit n${CYA}gin${NC}x config"
 	"8,66,${CYA}- - - - - - - - - - - - -${NC}"
@@ -135,7 +135,9 @@ qscreentext=(
 	"14,2,NGI - show ${CYA}ngi${NC}nx status"
 	"15,2,DOM - Show ${CYA}dom${NC}ain"
 	"12,66,${CYA}- - - - - - - - - - - - -${NC}"
-	"13,66,111:222:333 - ${CYA}men${NC}us"
+	"13,66,CER - ${CYA}Cer${NC}tificate expires"
+	"14,66,${CYA}- - - - - - - - - - - - -${NC}"
+	"15,66,111:222:333 - ${CYA}men${NC}us"
 	"16,2,BAL - ${CYA}bal${NC}ance Val address"
 	"17,2,CLA - show pending ${CYA}cla${NC}ims"
 	"18,2,NUM - ${CYA}num${NC}ber Vals & Apps"
@@ -148,11 +150,10 @@ qscreentext=(
 	"13,30,BRN - ${CYA}Br${NC}eak on error (${CYA}N${NC}O)"	
 	"14,30,AUT - ${CYA}Au${NC}to Restart (${CYA}T${NC}rue)"
         "15,30,AUF - ${CYA}Au${NC}to Restart (${CYA}F${NC}alse)"
+	"16,30,${CYA}- - - - - - - - - - - - -${NC}"
+	"17,30,SEN - ${CYA}Sen${NC}d pokt"
+        "18,30,T## - Terminal to node ##"
 	"18,66,X - ${RED}EXIT${NC}"
-	
-
-
-
 )
 }
 # ========================================
@@ -176,9 +177,7 @@ function drawqmenutext {
      line="${CGT}${posR};${posC}H $disc"
      echo -e "$line"
   done
-
 }
-
 # ========================================
 function drawlmenuframe {
 echo -e "${BON}l${DASH:1:90}k${BOF}"
@@ -197,7 +196,6 @@ do
 echo -e "\x1b(0x${THIRDS:1:90}x\x1b(B"
 done
 echo -e "\x1b(0m${DASH:1:90}j\x1b(B"
-
 }
 function drawlmenutext {
   for item in "${lscreentext[@]}"
@@ -215,40 +213,28 @@ function drawlmenutext {
 function createMenuStrings {
 #    tmenu="static"
     Lmenu=""
-    Fmenu=" \e[7m  ===== Beta Testing Menu ================= \e[0m
+    Fmenu=" \e[7m  ===== Screen & set-up Notes ================= \e[0m
     You may need to manually increese the screen size:
     Minimum screen size (100 colums 40 rows)
     Color test: ${RED}red ${GRN}green ${CYA}blue ${NC} none
     If you see:'qqqqqqqqqqq' instead of a streight line below:
     ${BON}qqqqqqqqqqq${BOF} set the terminal charset to: ISO-8859-16
 
-    all input is 3 character auto terminate no <enter> key
+    all menu choices are 3 character auto terminate no <enter> key
 
     3 screens available: 
-    111 = This, 222 = live monitor, 333 = development
+
+    111 = This, 
+    222 = live monitor, 
+    333 = Direct commands
     x to exit 
     
-    external validators must be set by hand (see top of this file)
+    External validators must be set by hand 
+    (see comments at the top of this file for examples)
 
-    NOTE: This whole 'Project' is a one-feature-at-a-time HACK JOB
-    Don't be shocked that the code looks ugly... it is ugly.
-    Please include with any comments (good or bad)
-    the Source Platform: EG: Win10, MacOS, etc.
-    the Terminal Software: EG: putty, iterm, x-term, etc.
-    the Server OS & version: EG: Ubuntu 20.04, Linux, etc.
 
-    Thank you for helping test this tool.
+    Thank you
     -BenVan
-    testing status:
-
-from: ubuntu 18.2 through: gnome_terminal to: ubuntu 20.04 Status: Rock and Roll!
-from: ubuntu 18.2 through: x-term to ubuntu 20.04 Status: NEARLY PERFECT.
-from: ubuntu 18.2 through: UXterm to ubuntu 20.04 Status: NEARLY PERFECT.
-from: ubuntu 18.2 through:  Putty to ubuntu 20.04 Status: NEARLY PERFECT.
-From: MacOS       through:  iTerm to Debian       Status: Good.
-From: IOS (Ipad)  through: Shelly to ubuntu 20.04 Status: NEARLY PERFECT.
-
-    
     "
   
    Cmenu=$Fmenu # set inital current menu to full menu
@@ -298,10 +284,10 @@ function fSta {
 function fRun {
         CurRUN=$(ps -C pocket | grep "pocket")
         CurRUN="${CurRUN:0:7}"
-
   	if [ "$CurRUN" != "" ]
   	then 
 		CurSTA="${GRN}Running local${NC}"
+		localVAL="local"
   	else 
 		CurSTA="${RED}Not Running${NC}"
 		if [ "$autoRestart" == "true" ]
@@ -334,7 +320,6 @@ function fRes {
         then
            CurSTA="        "
         fi
-
 }
 
 function fVal {
@@ -348,7 +333,7 @@ function fDom {
 	CurDOM=$(pocket query node $CurVAL |  grep -Po '((?<=service_url":.)|(?<=service_url":."))([^",\r\n]+)(?=[",\r\n]*)') || error_exit "No Domain for validator: $CurVAL @line $LINENO"
 }
 function fHei { 
-	CurHEI=$(pocket query height | grep -Po '((?<=height":.)|(?<=height":."))([^",\r\n]+)(?=[",\r\n]*)') || error_exit "Failed to grep height @line $LINENO"
+	CurHEI=$(pocket query height | grep -Po '((?<=height":.)|(?<=height":."))([^",\r\n]+)(?=[",\r\n]*)') || error_exit "Failed to grep height @line $LINENO";
 }
 function fBal {
 	  CurBAL=$(pocket query balance $CurVAL | grep -Po '((?<=balance":.)|(?<=balance":."))([^",\r\n]+)(?=[",\r\n]*)')  || error_exit "query balance failed @line $LINENO."
@@ -358,7 +343,6 @@ function fBal {
   		        Cur24B=$(pocket query balance $CurVAL $Cur24B | grep -Po '((?<=balance":.)|(?<=balance":."))([^",\r\n]+)(?=[",\r\n]*)') || error_exit "query balance at height failed @line $LINENO."
   		        let "Cur24B = $CurBAL - $Cur24B"
   	        fi
-
 }
 function fFir {
 	if [[ $EUID = 0 ]] 
@@ -372,7 +356,6 @@ function fFir {
 	then
            	CurFIR="        "
         fi
-
 }
 function fNgi {
 	CurNGI=$(service nginx status | grep "Active:") || error_exit "query nginx failed @line $LINENO."
@@ -381,8 +364,6 @@ function fNgi {
 	then
            CurNGI="        "
         fi
-
-
 }
 function fNet {
 	whatNET=$(cat .pocket/config/config.json | grep "Seeds") || error_exit "query seeds failed @line $LINENO."
@@ -396,8 +377,6 @@ function fNet {
 	then
            CurNET="        "
         fi
-
-
 }
 function fNod {
         CurNOD=$(pocket query node $CurVAL | grep "jailed") || error_exit "query node:$CurVAL: failed @line $LINENO."
@@ -421,6 +400,11 @@ function fCla {
 		CurCLB=("${CurCLAp[@]}")	       
 	fi	
 }
+function fCer {
+	echo -ne "${CGT}1;1H"
+	echo | openssl s_client -servername ${CurDOM} -connect ${CurDOM}:443 2>/dev/null | openssl x509 -noout -dates | grep 'After'
+	echo $Expire
+}
 function fStk {
 	CurSTK=$(pocket query node $CurVAL | grep -Po '((?<=status":.)|(?<=status":."))([^",\r\n]+)(?=[",\r\n]*)') || error_exit "query node failed @line $LINENO."
 	case $CurSTK in
@@ -428,7 +412,6 @@ function fStk {
 	        2 ) CurSTK="${GRN}true${NC}";;
 	        * ) CurSTK="${RED}false${NC}";;
 	esac 
-
 }
 function fVvv {
 	CurVVV=$eVALS
@@ -461,7 +444,9 @@ function fChj {
                 fi
                 if [ "$fullText" == "full" ]
 		then
+		  ts=$(date +%s%N)
 		  echo $(curl -s -m 2 -XPOST "${CurCHU[$i]}/v1/query/height")
+                  echo "Round trip to Pocket in mili-seconds: $((($(date +%s%N) - $ts)/1000000))"
 		fi
 	  ;;
 	  0021 ) EthBlockHeight=""
@@ -474,18 +459,17 @@ function fChj {
 		fi
 		if [ "$fullText" == "full" ]
 		then
+		  ts=$(date +%s%N)
 		  echo $(curl -s -m 3 POST --insecure -H "Content-Type: application/json" --data '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":64}' ${CurCHU[$i]})
+                  echo "Round trip to ETH in mili-seconds: $((($(date +%s%N) - $ts)/1000000))"
 		fi
 	  ;;
 	  esac
         done
 }
 function fE00 {
-
         CurEHEI=$(curl -s -m 2 http://$1:26657/status | grep -Po '((?<=latest_block_height":.)|(?<=latest_block_height":."))([^",\r\n]+)(?=[",\r\n]*)')
-
 }
-
 
 function fN00 {
     	valChoice=${selection:1:2}
@@ -500,14 +484,14 @@ function fN00 {
       		if [ "$valChoice" == "${a[1]}" ] || [ "$1" == "all" ] 
       		then
 	     		CurVAL="${a[0]}"
-			Eurl="${a[3]}"
+			Eurl="${a[4]}"
 			fE00 $Eurl
 			fNod
 			for i in "${!eVALS[@]}"; 
 			do
 				if [ "${eVALS[$i]}" == "${item}" ] 
 				then
-				eVALS[$i]="${a[0]},${a[1]},${a[2]},${a[3]},$CurEHEI,$CurNOD"
+				eVALS[$i]="${a[0]},${a[1]},${a[2]},${a[3]},${a[4]},$CurEHEI,$CurNOD}"
 				localVAL="external"
 				CurVAL="${a[0]}"
 				fi
@@ -529,11 +513,96 @@ function fN00 {
 		fi
     	done
 }
+function fT00 {
+        valChoice=${selection:1:2}
+        for item in "${eVALS[@]}"
+        do
+                readarray -td, a <<<"$item,"; unset 'a[-1]';
+                if [ "$valChoice" == "${a[1]}" ]
+                then
+			Turl="${a[4]}"
+			Tusr="${a[3]}"
+                        ssh "$Tusr@$Turl"
+                fi
+        done
+}
 function f0001 {
 # nothing here yet
 echo ""
 }
+function fSen {
+	j=0
+	posC=0
+	line="${CGT}${j};${posC}H ${NC}"
+	out=$(f=${CurBAL#*[,]} printf "%'.${#f}f\n" "$CurBAL")
+	line+="${GRN}Send From:${NC} $CurVAL  ${GRN}Available Balance = ${NC}$out"
+	echo -e $line;
+	j=2
+	posC=0
+          for i in "${eVALS[@]}";
+          do
+            readarray -td, v <<<"$i,"; unset 'v[-1]';
+            line="${CGT}${j};${posC}H ${NC}"
+	    ac="${v[0]:0:6}.... ";
+            aa="${v[1]}";
+            ab="${v[2]}";
 
+	    line+="${CYA}$aa ${NC}$ab $ac";
+            echo -e "$line"
+
+	    posC=$((posC+22))
+	    if [ $posC = 88 ]
+	    then 
+		    j=$((j+1));
+		    posC=0;
+	    fi
+	  done
+
+#  - - - - - - - add the NOT back in to test on a real vaidator != - - - -
+        if [ "$localVAL" != "local" ];
+        then
+           echo "Can only send from local validator.. change node first please."
+	   sleep 5
+	else
+		echo ""
+		echo -ne "${GRN}Choose Destination (##): ${NC}"
+    	read  toIndex
+            ac="";
+            ab="";
+          for i in "${eVALS[@]}";
+          do
+            readarray -td, v <<<"$i,"; unset 'v[-1]';
+            ad="${v[1]}";
+            if [ "$toIndex" == "$ad" ]
+            then
+            aa="${v[1]}";
+            ac="${v[0]}";
+            ab="${v[2]}";
+            fi
+          done
+	echo -ne "${GRN}Amount in full POKT (leave off the last 6 digits) : ${NC}"
+		read  toAmount
+		echo -ne "${GRN}note : ${NC}"
+		read note
+		echo -e "${CYA}using default fee of 10,000 uPOKT"
+
+		echo "Send from: $CurVAL"
+	    	echo "To: $ab - $ac"
+		echo -e "Amount of: $toAmount POKT plus fee of 10000 uPOKT${NC}"
+#		echo "pocket accounts send-tx $CurVAL $ac ${toAmount}000000 mainnet 10000 $note"
+		echo -ne "${GRN}Continue with passphrase: (Y/N)?: ${NC}"
+		read YorN
+		YorN=${YorN^^}
+		if [ "$YorN" == "Y" ]
+		then
+		       echo -ne "${GRN}enter passphrase: ${NC}"	
+			result=$(pocket accounts send-tx $CurVAL $ac ${toAmount}000000 mainnet 10000 $note)
+			echo -ne "$result ${NC}"
+                fi
+	echo -e "Done${NC} .. Press <enter>"
+	read YorN
+	fi
+}
 # ========================================================================
 function loadStatusValues {
 
@@ -597,8 +666,8 @@ if [ "$d" == "live" ]; then
 	    eVadd="${v[0]}";
             eVref="${v[1]}";
             eVnic="${v[2]}";
-	    eVhei="${v[4]}";
-	    eVnod="${v[5]}";
+	    eVhei="${v[5]}";
+	    eVnod="${v[6]}";
 	    if [[ "$eVhei" ==  "$CurHEI" ]]; then
 		    eVhei="${GRN}$eVhei${NC}"
             fi
@@ -673,9 +742,6 @@ if [ "$d" == "live" ]; then
           done
           echo -e "$line"
           ;;
-
-
-
 	* )  # ====== ===== ===== all other types==== ===== ==== ====== ==== === ====
           line+="${CYA}${!Cur}${NC}"
 	  tCur=${!Cur}
@@ -739,6 +805,16 @@ until [ "$selection" == "X" ]; do
 	SIM ) $(pocket start --simulateRelay >/dev/null &)
 	        echo -e "${CYA}pocket simuateRelay started in background${NC}"
 	;;
+	SEN ) fSen
+		echo -e "${releaseScrollArea}"
+	 Cmenu="";    tmenu="query"; echo -e ${CGH}${CCD};
+                  drawqmenuframe
+                  drawqmenutext
+                  showqmenu nothing all $tmenu
+                echo -e "${setScrollArea}"
+                echo -e "${lockScrollArea}"
+                echo -e "${CGT}30;3H"
+	;;
 	AUT ) autoRestart="true"
                 echo -e "Auto Restart :${CYA}$autoRestart${NC}"
 	;;
@@ -758,6 +834,8 @@ until [ "$selection" == "X" ]; do
 	;;
         CON )   vi .pocket/config/config.json
 	;;
+	CER )  fCer
+		;;
         GIN )   vi /etc/nginx/sites-available/pocket-proxy.conf
 	;;
         VAL ) fVal
@@ -824,9 +902,21 @@ until [ "$selection" == "X" ]; do
                    loadStatusValues
                    showlmenu nothing all $tmenu
         ;;
-
+	N20|N21|N22|N23|N24|N25|N26|N27|N28|N29|N30 ) fN00
+                   loadStatusValues
+                   showlmenu nothing all $tmenu
+        ;;
 	N99 ) fN00 all
                    loadStatusValues
+                   showlmenu nothing all $tmenu
+        ;;
+        T00|T01|T02|T03|T04|T05|T06|T07|T08|T09 ) fT00
+                   showlmenu nothing all $tmenu
+        ;;
+        T10|T11|T12|T13|T14|T15|T16|T17|T18|T19 ) fT00
+                   showlmenu nothing all $tmenu
+        ;;
+        T20|T21|T22|T23|T24|205|T26|T27|T28|T29|T30 ) fT00
                    showlmenu nothing all $tmenu
         ;;
 	DOM )  fVal
@@ -875,27 +965,5 @@ done
     setTheBlockTimer
     createMenuStrings
     initWindow
-    if [ "$CurSTA" == "Not Running" ]
-    then 
-	echo "============== Warning: Pocket is not currently running ========"
-	echo ""
-        echo "That is OK...     UNLESS...  you have a custom data directory"
-        echo ""
-	echo "If your set-up uses a custom data directory..."
-	echo ""
-	echo " STOP THIS PROCESS NOW   < CONTROL > C "
-	echo ""
-	echo " and start Pocket from the command line with the --DataDirectory option"
-	echo ""
-	echo " and DO NOT USE the start or start-simulated or features of this script!!"
-	echo ""
-        echo ""
-        echo "Continue (y/N):"	
-        read -n1 yn
-        yn=${yn^^}
-        if [ "$yn" != "Y" ]	
-	then
-		exit
-	fi
-    fi
     main
+
